5fa23e19b9db27253728a2743b73f1f280477c93
XuanYi0905 <xuanyi2209@nutc.edu.tw>
2024-11-08 12:09:44 +0800

    20241108

--- AgencyProject/js/bs5vue.js
+++ AgencyProject/js/bs5vue.js
@@ -1,26 +1,24 @@
 const {createApp,ref}  = Vue;
 
-createApp({
+var myService = createApp({
     data() {
         return{
             Services:[
                 {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "lock", heading:""}
+                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
             ]
         }
     }
 }).mount("#services");
 
+
+myService.Services.push({icon: "fa-lock", heading:"Web Security", text:"asdf adf adf"})
+
 createApp({
     data() {
         return{
             Portfolio:[
-                {,,,},
-                {},
-                {},
-                {},
-                {},
+                {modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"},
                 {}
             ]
         }
