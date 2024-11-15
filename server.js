b9079dd8834e49b3a7e6638912cb7962d99ffa28
XuanYi0905 <xuanyi2209@nutc.edu.tw>
2024-11-15 11:18:46 +0800

    20241115

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
+
+server.get("/services", (req, res)=>{
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+    ];
+    res.send(Services);
+})
+
+server.get("/portfolio", (req, res)=>{
+
+    res.send("portfolio");
+
+})
+
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
 })
