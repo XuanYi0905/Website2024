2dcac9592fd74e6e7ba8aad98143522be2a135d8
XuanYi0905 <xuanyi2209@nutc.edu.tw>
2024-12-27 11:15:28 +0800

    20241227

---
+++ Final/js/vue.js
@@ -0,0 +1,32 @@
+const {createApp,ref}  = Vue;
+
+var Pajamas = createApp({
+    data() {
+        return{
+            Portfolio:[
+            { imgSrc: "images/t12.jpg", hoverImg: "images/t32.jpg", heading: "Pajama 1", price: "2000 NTD" },
+            { imgSrc: "images/t13.jpg", hoverImg: "images/t48.jpg", heading: "Pajama 2", price: "2000 NTD" },
+            { imgSrc: "images/t14.jpg", hoverImg: "images/t47.jpg", heading: "Pajama 3", price: "2000 NTD" },
+            { imgSrc: "images/t17.jpg", hoverImg: "images/t35.jpg", heading: "Pajama 4", price: "2000 NTD" }
+            ]
+        }
+    }
+}).mount("#pajamas")
+
+var vueProfolio = createApp({
+    data() {
+        return{
+            Pajamas:[]
+        }
+    }
+}).mount("#pajamas")
+
+$.ajax({
+    url:"/pajamas",
+    method: "get",
+    dataType: "json",
+    success: results=>{
+        vueProfolio.Pajamas = results;
+    }
+
+})
