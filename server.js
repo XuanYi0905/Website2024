1775e77419885254780feb40e0861995d0a2ce8d
XuanYi0905 <xuanyi2209@nutc.edu.tw>
2025-01-03 11:51:14 +0800

    20250103

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
 
-server.use(express.static(__dirname + "/.."));
+server.use(express.static(__dirname + "/Final"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded());
 
 var DB = require("nedb-promises");
-var PajamasDB = DB.create(__dirname + "/../pajamas.db");
-var ContactDB = DB.create(__dirname+"/../contact.db");
+var PajamasDB = DB.create(__dirname + "/Final/pajamas.db");
+var ContactDB = DB.create(__dirname+"/Final/contact.db");
 
 PajamasDB.insert([
   { imgSrc: "images/t12.jpg", hoverImg: "images/t32.jpg", heading: "Pajama 1", price: "2000 NTD" },
